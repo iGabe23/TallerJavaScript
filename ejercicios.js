30239942bb2cf7dcb47e89504358af22115b23bd
Gabriel Martinez <2003gabrielmh@gmail.com>
2023-11-17 22:11:11 -0500

    ejercicios

--- ejercicios.js
+++ ejercicios.js
@@ -2,6 +2,8 @@
 
 /*Preguntar por nombre al usuario y saluder por nombre.*/
 
+//Constante definida con un método de entrar: prompt
+
 /* const nombre = prompt("Hola, cómo te llamas?");
 document.write("Bienvenido, " + nombre + ".");
 */
@@ -9,9 +11,19 @@ document.write("Bienvenido, " + nombre + ".");
 /* Prguntar por nombre al usuario y saluder por nombre.
 Si el usuario no escribe su nombre, saludar: "Hola, desconocido." */
 
+// Condicional ifelse
+/*
 const nombre = prompt("Hola, cómo te llamas?");
 if (nombre) {
   document.write("Hola, " + nombre);
 } else {
   document.write("Hola, desconocido.");
 }
+*/
+// Usar una funcion
+
+function saludar(nombre = " Desconocido") {
+  document.write("Hola, " + nombre);
+}
+
+saludar(prompt("Cual es su nombre?"));
